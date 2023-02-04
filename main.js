0a054e0ce16831394bfdb4b37f5beb2814226333
theimaginedseven <103391499+theimaginedseven@users.noreply.github.com>
2023-02-05 09:30:23 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status = "";
+
+function setup(){
+    canvas = createCanvas(480, 380)
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+}
